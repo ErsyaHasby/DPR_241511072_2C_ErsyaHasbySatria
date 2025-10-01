217b4c57a3e24786def3c0673f52518975dec865
Ersya Hasby Satria <hasbysatria54@gmail.com>
2025-10-01 15:14:24 +0700

    Add Fitur Login dan Logout by Ersya Hasby Satria

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -2,7 +2,10 @@
 
 use CodeIgniter\Router\RouteCollection;
 
-/**
- * @var RouteCollection $routes
- */
-$routes->get('/', 'Home::index');
+/** @var RouteCollection $routes */
+$routes->get('/', 'AuthController::login');
+$routes->get('/login', 'AuthController::login');
+$routes->post('/auth/processLogin', 'AuthController::processLogin');
+$routes->get('/auth/logout', 'AuthController::logout');
+$routes->get('/dashboard', 'AuthController::dashboard');
+$routes->get('/testdb', 'TestDbController::index');

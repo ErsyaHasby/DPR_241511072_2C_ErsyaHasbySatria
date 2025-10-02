e0252c6b06c454c97e287484d5affb0374c5c2b8
Ersya Hasby Satria <hasbysatria54@gmail.com>
2025-10-02 09:04:29 +0700

    Add Fitur Tambah Data Anggota by Ersya Hasby Satria

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -4,6 +4,10 @@ $routes->get('/login', 'AuthController::login');
 $routes->post('/auth/doLogin', 'AuthController::doLogin');
 $routes->get('/logout', 'AuthController::logout');
 $routes->get('/dashboard', 'AuthController::dashboard');
+$routes->group('anggota', ['filter' => 'admin', 'namespace' => 'App\Controllers'], function ($routes) {
+    $routes->get('create', 'AnggotaController::create');
+    $routes->post('store', 'AnggotaController::store');
+});
 $routes->get('/unauthorized', function () {
     return view('errors/unauthorized');
 });

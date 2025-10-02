410c1b30c1ce973c2284cb1116a10696561827b3
Ersya Hasby Satria <hasbysatria54@gmail.com>
2025-10-02 11:19:10 +0700

    Add Tambah Data Anggota by Ersya Hasby Satria

---
+++ app/Controllers/AnggotaController.php
@@ -0,0 +1,47 @@
+<?php
+namespace App\Controllers;
+use CodeIgniter\Controller;
+use App\Models\AnggotaModel;
+
+class AnggotaController extends Controller
+{
+    protected $model;
+
+    public function __construct()
+    {
+        $this->model = new AnggotaModel();
+    }
+
+    public function index()
+    {
+        $keyword = $this->request->getGet('keyword');
+        $data['anggota'] = $keyword ? $this->model->search($keyword)->findAll() : $this->model->findAll();
+        $data['title'] = 'Data Anggota DPR';
+        return view('anggota/index', $data);
+    }
+
+    public function create()
+    {
+        $data['title'] = 'Tambah Anggota DPR';
+        return view('anggota/create', $data);
+    }
+
+    public function store()
+    {
+        if (
+            !$this->validate([
+                'nama_depan' => 'required|alpha_space',
+                'nama_belakang' => 'required|alpha_space',
+                'gelar_depan' => 'permit_empty|alpha_space',
+                'gelar_belakang' => 'permit_empty|alpha_space',
+                'jabatan' => 'required|in_list[Ketua,Wakil Ketua,Anggota]',
+                'status_pernikahan' => 'required|in_list[Kawin,Belum Kawin,Cerai Hidup,Cerai Mati]',
+                'jumlah_anak' => 'permit_empty|integer|greater_than_equal_to[0]'
+            ])
+        ) {
+            return redirect()->back()->withInput()->with('errors', $this->validator->getErrors());
+        }
+        $this->model->insert($this->request->getPost());
+        return redirect()->to('/anggota')->with('success', 'Anggota ditambahkan');
+    }
+}

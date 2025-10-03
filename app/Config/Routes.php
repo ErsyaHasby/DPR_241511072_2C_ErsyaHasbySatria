17c25c75f4e8a43119bd9b49a093390d14afbc75
Ersya Hasby Satria <hasbysatria54@gmail.com>
2025-10-03 11:07:05 +0700

    add ci4 master new

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -2,10 +2,7 @@
 
 use CodeIgniter\Router\RouteCollection;
 
-/** @var RouteCollection $routes */
-$routes->get('/', 'AuthController::login');
-$routes->get('/login', 'AuthController::login');
-$routes->post('/auth/processLogin', 'AuthController::processLogin');
-$routes->get('/auth/logout', 'AuthController::logout');
-$routes->get('/dashboard', 'AuthController::dashboard');
-$routes->get('/testdb', 'TestDbController::index');
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
